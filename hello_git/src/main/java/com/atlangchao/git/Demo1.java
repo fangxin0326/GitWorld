f78d466ffddf0bc99dde1c22e4f68c13144b9ad2
fangxin <514063663@qq.com>
2020-08-02 12:48:17 +0800

    更新1

--- hello_git/src/main/java/com/atlangchao/git/Demo1.java
+++ hello_git/src/main/java/com/atlangchao/git/Demo1.java
@@ -7,5 +7,7 @@ package com.atlangchao.git;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("更新1");
+
     }
 }
