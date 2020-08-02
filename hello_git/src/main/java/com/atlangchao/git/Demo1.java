e42ff784d171bfc30ab737e6b4ff15719a1f2208
fangxin <514063663@qq.com>
2020-08-02 13:13:01 +0800

    创建分支

--- hello_git/src/main/java/com/atlangchao/git/Demo1.java
+++ hello_git/src/main/java/com/atlangchao/git/Demo1.java
@@ -8,6 +8,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("更新1");
+        System.out.println("创建分支");
 
     }
 }
