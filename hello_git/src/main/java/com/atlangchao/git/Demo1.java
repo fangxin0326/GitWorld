cf2c4f16c7d46bd1238270a04605e7dd410d1e87
fangxin <514063663@qq.com>
2020-08-02 22:56:36 +0800

    test merge

--- hello_git/src/main/java/com/atlangchao/git/Demo1.java
+++ hello_git/src/main/java/com/atlangchao/git/Demo1.java
@@ -6,12 +6,13 @@ package com.atlangchao.git;
  */
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello github");
         System.out.println("更新1");
         System.out.println("创建分支");
         System.out.println("主干");
-        System.out.println("合并分支");
-        System.out.println("最新更新");
+        System.out.println("合并主干分支");
+        System.out.println("最新更新11");
         System.out.println("test");
+        System.out.println("merge");
     }
 }
