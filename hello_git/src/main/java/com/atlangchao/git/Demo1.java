a71d98018dd6b9e54fa65a3c889d9af2ffff55c3
fangxin <514063663@qq.com>
2020-08-02 13:32:27 +0800

    合并分支

--- hello_git/src/main/java/com/atlangchao/git/Demo1.java
+++ hello_git/src/main/java/com/atlangchao/git/Demo1.java
@@ -9,6 +9,7 @@ public class Demo1 {
         System.out.println("Hello git");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("合并分支");
 
     }
 }
