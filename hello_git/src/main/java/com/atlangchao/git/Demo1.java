ad3fcb097edac2c7285fd448f2b64f2fc3399b63
fangxin <514063663@qq.com>
2020-08-02 12:44:26 +0800

    添加文件

---
+++ hello_git/src/main/java/com/atlangchao/git/Demo1.java
@@ -0,0 +1,11 @@
+package com.atlangchao.git;
+
+/**
+ * @author fangxin
+ * @create 2020-08-02 12:00
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
