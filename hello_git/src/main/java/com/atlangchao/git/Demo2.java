f8634c716160eb485e1b92c5e3f414627b143574
fangxin <514063663@qq.com>
2020-08-03 20:24:53 +0800

    demo3

---
+++ hello_git/src/main/java/com/atlangchao/git/Demo2.java
@@ -0,0 +1,11 @@
+package com.atlangchao.git;
+
+/**
+ * @author fangxin
+ * @create 2020-08-03 20:23
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("做 test 测试");
+    }
+}
